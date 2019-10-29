12206019469056aaef56598cf6aa9f93466f995f
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-29 22:08:39 +0900

    reverted

--- parallel_wavegan/utils/dataset.py
+++ parallel_wavegan/utils/dataset.py
@@ -14,24 +14,14 @@ from nnmnkwii.datasets import FileSourceDataset
 class BaseDataSource(FileDataSource):
     """Base class of data source."""
 
-    def __init__(self, dump_root, query="*.npy", min_length=None):
+    def __init__(self, dump_root, query="*.npy"):
         """Initialize base data source."""
         self.dump_root = dump_root
         self.query = query
-        self.min_length = min_length
 
     def collect_files(self):
         """Collect all of the files."""
         paths = sorted(glob.glob(os.path.join(self.dump_root, self.query)))
-        lengths = [self.collect_features(path).shape[0] for path in paths]
-        # remove short utterances
-        if self.min_length is not None:
-            idxs = [idx for idx, l in enumerate(lengths) if l >= self.min_length]
-            if len(idxs) != len(paths):
-                print(f"short utterances are removed ({len(paths)} -> {len(idxs)}).")
-            paths = [paths[idx] for idx in idxs]
-            lengths = [lengths[idx] for idx in idxs]
-        self.lengths = lengths
         return paths
 
     def collect_features(self, path):
@@ -42,49 +32,47 @@ class BaseDataSource(FileDataSource):
 class RawAudioDataSource(BaseDataSource):
     """Raw audio data source."""
 
-    def __init__(self, dump_root, min_length=None):
+    def __init__(self, dump_root):
         """Initialize Raw audio data source."""
-        super(RawAudioDataSource, self).__init__(dump_root, "*-wave.npy", min_length)
+        super(RawAudioDataSource, self).__init__(dump_root, "*-wave.npy")
 
 
 class MelSpecDataSource(BaseDataSource):
     """Mel spectrogram data source."""
 
-    def __init__(self, dump_root, min_length=None):
+    def __init__(self, dump_root):
         """Initialize Mel spectrogram data source."""
-        super(MelSpecDataSource, self).__init__(dump_root, "*-feats.npy", min_length)
+        super(MelSpecDataSource, self).__init__(dump_root, "*-feats.npy")
 
 
 class RawAudioDataset(FileSourceDataset):
     """Raw audio dataset."""
 
-    def __init__(self, dump_root, min_length=None):
+    def __init__(self, dump_root):
         """Initialize Raw audio dataset."""
-        data_source = RawAudioDataSource(dump_root, min_length)
+        data_source = RawAudioDataSource(dump_root)
         super(RawAudioDataset, self).__init__(data_source)
 
 
 class MelSpecDataset(FileSourceDataset):
     """Raw audio dataset."""
 
-    def __init__(self, dump_root, min_length=None):
+    def __init__(self, dump_root):
         """Initialize Raw audio dataset."""
-        data_source = MelSpecDataSource(dump_root, min_length)
+        data_source = MelSpecDataSource(dump_root)
         super(MelSpecDataset, self).__init__(data_source)
 
 
 class PyTorchDataset(object):
     """PyTorch compatible dataset."""
 
-    def __init__(self, dump_root, audio_min_length=None, mel_min_length=None):
+    def __init__(self, dump_root):
         """Initialize pytroch dataset."""
-        audio_dataset = RawAudioDataset(dump_root, audio_min_length)
-        mel_dataset = MelSpecDataset(dump_root, mel_min_length)
+        audio_dataset = RawAudioDataset(dump_root)
+        mel_dataset = MelSpecDataset(dump_root)
         assert len(audio_dataset) == len(mel_dataset)
         self.audio_dataset = audio_dataset
         self.mel_dataset = mel_dataset
-        self.audio_lengths = np.array(audio_dataset.file_data_source.lengths)
-        self.mel_lengths = np.array(mel_dataset.file_data_source.lengths)
 
     def __getitem__(self, idx):
         """Get specifed idx items.
