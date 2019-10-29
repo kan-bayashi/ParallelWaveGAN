2b72fc9316192308cc506aeb3f949d4e57d39554
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-29 16:39:37 +0900

    removed

--- parallel_wavegan/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-from __future__ import with_statement, print_function, absolute_import
-
-from .version import version as __version__
-
-from .wavenet import receptive_field_size, WaveNet
