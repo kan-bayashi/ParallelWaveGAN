2ffa7e0e77044c24cb57d13efd3bc5dd94551587
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-29 17:57:01 +0900

    updated test

--- test/test_parallel_wavegan.py
+++ test/test_parallel_wavegan.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import pytest
 import torch
 import torch.nn.functional as F
 
@@ -10,22 +12,89 @@ from parallel_wavegan.models import ParallelWaveGANGenerator
 from parallel_wavegan.optimizers import RAdam
 
 
-def test_parallel_wavegan_trainable():
+def make_generator_args(**kwargs):
+    defaults = dict(
+        in_channels=1,
+        out_channels=1,
+        kernel_size=3,
+        layers=6,
+        stacks=3,
+        residual_channels=8,
+        gate_channels=16,
+        skip_channels=8,
+        aux_channels=10,
+        dropout=1 - 0.95,
+        use_weight_norm=True,
+        upsample_conditional_features=True,
+        upsample_net="ConvInUpsampleNetwork",
+        upsample_params={"upsample_scales": [4, 4]},
+    )
+    defaults.update(kwargs)
+    return defaults
+
+
+def make_discriminator_args(**kwargs):
+    defaults = dict(
+        in_channels=1,
+        out_channels=1,
+        kernel_size=3,
+        layers=5,
+        conv_channels=16,
+        nonlinear_activation="LeakyReLU",
+        nonlinear_activation_params={"negative_slope": 0.2},
+        bias=True,
+        use_weight_norm=True,
+    )
+    defaults.update(kwargs)
+    return defaults
+
+
+def make_mutli_reso_stft_loss_args(**kwargs):
+    defaults = dict(
+        fft_sizes=[64, 128, 256],
+        shift_sizes=[32, 64, 128],
+        window_lengths=[48, 96, 192],
+        window_type='hann_window',
+    )
+    defaults.update(kwargs)
+    return defaults
+
+
+@pytest.mark.parametrize(
+    "dict_g, dict_d, dict_loss", [
+        ({}, {}, {}),
+        ({"layers": 1, "stacks": 1}, {}, {}),
+        ({}, {"layers": 1}, {}),
+        ({"kernel_size": 5}, {}, {}),
+        ({}, {"kernel_size": 5}, {}),
+        ({"gate_channels": 8}, {}, {}),
+        ({"stacks": 1}, {}, {}),
+        ({"use_weight_norm": False}, {"use_weight_norm": False}, {}),
+        ({"upsample_params": {"upsample_scales": [4], "aux_context_window": 1}}, {}, {}),
+        ({"upsample_params": {"upsample_scales": [4], "freq_axis_kernel_size": 3}}, {}, {}),
+        ({"upsample_conditional_features": False, "upsample_params": {"upsample_scales": [1]}}, {}, {}),
+        ({}, {"nonlinear_activation": "ReLU", "nonlinear_activation_params": {}}, {}),
+    ])
+def test_parallel_wavegan_trainable(dict_g, dict_d, dict_loss):
     # setup
     batch_size = 4
     batch_length = 4096
+    args_g = make_generator_args(**dict_g)
+    args_d = make_discriminator_args(**dict_d)
+    args_loss = make_mutli_reso_stft_loss_args(**dict_loss)
     z = torch.randn(batch_size, 1, batch_length)
-    c = torch.randn(batch_size, 80, batch_length // 256)
     y = torch.randn(batch_size, 1, batch_length)
-    generator = ParallelWaveGANGenerator()
-    discriminator = ParallelWaveGANDiscriminator()
-    aux_criterion = MultiResolutionSTFTLoss()
-    optimizer_g = RAdam(generator.parameters())
-    optimizer_d = RAdam(discriminator.parameters())
+    c = torch.randn(batch_size, args_g["aux_channels"],
+                    batch_length // np.prod(args_g["upsample_params"]["upsample_scales"]))
+    model_g = ParallelWaveGANGenerator(**args_g)
+    model_d = ParallelWaveGANDiscriminator(**args_d)
+    aux_criterion = MultiResolutionSTFTLoss(**args_loss)
+    optimizer_g = RAdam(model_g.parameters())
+    optimizer_d = RAdam(model_d.parameters())
 
     # check generator trainable
-    y_hat = generator(z, c)
-    p_hat = discriminator(y_hat)
+    y_hat = model_g(z, c)
+    p_hat = model_d(y_hat)
     y, y_hat, p_hat = y.squeeze(1), y_hat.squeeze(1), p_hat.squeeze(1)
     adv_loss = F.mse_loss(p_hat, p_hat.new_ones(p_hat.size()))
     aux_loss = aux_criterion(y_hat, y)
@@ -36,8 +105,8 @@ def test_parallel_wavegan_trainable():
 
     # check discriminator trainable
     y, y_hat = y.unsqueeze(1), y_hat.unsqueeze(1).detach()
-    p = discriminator(y)
-    p_hat = discriminator(y_hat)
+    p = model_d(y)
+    p_hat = model_d(y_hat)
     p, p_hat = p.squeeze(1), p_hat.squeeze(1)
     loss_d = F.mse_loss(p, p.new_ones(p.size())) + F.mse_loss(p_hat, p_hat.new_zeros(p_hat.size()))
     optimizer_d.zero_grad()
