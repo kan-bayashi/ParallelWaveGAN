9fddff30139b4ce4ad7de88a5a17de4b4132316e
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-30 15:46:36 +0900

    added length_threshold options

--- parallel_wavegan/utils/dataset.py
+++ parallel_wavegan/utils/dataset.py
@@ -3,6 +3,7 @@
 """Dataset related classes."""
 
 import glob
+import logging
 import os
 
 import numpy as np
@@ -14,15 +15,22 @@ from nnmnkwii.datasets import FileSourceDataset
 class BaseDataSource(FileDataSource):
     """Base class of data source."""
 
-    def __init__(self, dump_root, query="*.npy"):
+    def __init__(self, dump_root, query="*.npy", length_threshold=None):
         """Initialize base data source."""
         self.dump_root = dump_root
         self.query = query
+        self.length_threshold = length_threshold
 
     def collect_files(self):
         """Collect all of the files."""
         paths = sorted(glob.glob(os.path.join(self.dump_root, self.query)))
-        return paths
+        if self.length_threshold is not None:
+            new_paths = [p for p in paths if self.collect_features(p).shape[0] > self.length_threshold]
+            if len(paths) != len(new_paths):
+                logging.warn(f"short samples are remvoed ({len(paths)} -> {len(new_paths)}).")
+            return new_paths
+        else:
+            return paths
 
     def collect_features(self, path):
         """Collect feature from path."""
@@ -32,44 +40,44 @@ class BaseDataSource(FileDataSource):
 class RawAudioDataSource(BaseDataSource):
     """Raw audio data source."""
 
-    def __init__(self, dump_root):
+    def __init__(self, dump_root, audio_length_threshold=None):
         """Initialize Raw audio data source."""
-        super(RawAudioDataSource, self).__init__(dump_root, "*-wave.npy")
+        super(RawAudioDataSource, self).__init__(dump_root, "*-wave.npy", audio_length_threshold)
 
 
 class MelSpecDataSource(BaseDataSource):
     """Mel spectrogram data source."""
 
-    def __init__(self, dump_root):
+    def __init__(self, dump_root, mel_length_threshold=None):
         """Initialize Mel spectrogram data source."""
-        super(MelSpecDataSource, self).__init__(dump_root, "*-feats.npy")
+        super(MelSpecDataSource, self).__init__(dump_root, "*-feats.npy", mel_length_threshold)
 
 
 class RawAudioDataset(FileSourceDataset):
     """Raw audio dataset."""
 
-    def __init__(self, dump_root):
+    def __init__(self, dump_root, audio_length_threshold=None):
         """Initialize Raw audio dataset."""
-        data_source = RawAudioDataSource(dump_root)
+        data_source = RawAudioDataSource(dump_root, audio_length_threshold)
         super(RawAudioDataset, self).__init__(data_source)
 
 
 class MelSpecDataset(FileSourceDataset):
     """Raw audio dataset."""
 
-    def __init__(self, dump_root):
+    def __init__(self, dump_root, mel_length_threshold=None):
         """Initialize Raw audio dataset."""
-        data_source = MelSpecDataSource(dump_root)
+        data_source = MelSpecDataSource(dump_root, mel_length_threshold)
         super(MelSpecDataset, self).__init__(data_source)
 
 
 class PyTorchDataset(object):
     """PyTorch compatible dataset."""
 
-    def __init__(self, dump_root):
+    def __init__(self, dump_root, audio_length_threshold=None, mel_length_threshold=None):
         """Initialize pytroch dataset."""
-        audio_dataset = RawAudioDataset(dump_root)
-        mel_dataset = MelSpecDataset(dump_root)
+        audio_dataset = RawAudioDataset(dump_root, audio_length_threshold)
+        mel_dataset = MelSpecDataset(dump_root, mel_length_threshold)
         assert len(audio_dataset) == len(mel_dataset)
         self.audio_dataset = audio_dataset
         self.mel_dataset = mel_dataset
