ffd469f2192b80063658feb74ca4b82dfdcc5be5
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-11-04 14:26:28 +0900

    added downloader

---
+++ egs/arctic/voc1/local/data_download.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+# Copyright 2019 Nagoya University (Tomoki Hayashi)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+download_dir=$1
+spk=$2
+
+available_spks=(
+    "slt" "clb" "bdl" "rms" "jmk" "awb" "ksp"
+)
+
+# check arguments
+if [ $# != 2 ]; then
+    echo "Usage: $0 <download_dir> <spk>"
+    echo "Available speakers: ${available_spks[*]}"
+    exit 1
+fi
+
+# check speakers
+if ! echo "${available_spks[*]}" | grep -q "${spk}"; then
+    echo "Specified spk (${spk}) is not available or not supported." >&2
+    exit 1
+fi
+
+# download dataset
+cwd=$(pwd)
+if [ ! -e "${download_dir}/cmu_us_${spk}_arctic" ]; then
+    mkdir -p "${download_dir}"
+    cd "${download_dir}"
+    wget "http://festvox.org/cmu_arctic/cmu_arctic/packed/cmu_us_${spk}_arctic-0.95-release.tar.bz2"
+    tar xf "cmu_us_${spk}_arctic-0.95-release.tar.bz2"
+    rm "cmu_us_${spk}_arctic-0.95-release.tar.bz2"
+    cd "${cwd}"
+    echo "Successfully finished download."
+else
+    echo "Already exists. Skip download."
+fi
