4d4e36b88734e16569c3bac774fdf4794c29c2f0
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-01-14 13:25:05 +0900

    cleaned

--- parallel_wavegan/models/parallel_wavegan.py
+++ parallel_wavegan/models/parallel_wavegan.py
@@ -18,7 +18,7 @@ from parallel_wavegan.layers import upsample
 
 class ParallelWaveGANGenerator(torch.nn.Module):
     """Parallel WaveGAN Generator module."""
-    
+
     def __init__(self,
                  in_channels=1,
                  out_channels=1,
@@ -66,14 +66,14 @@ class ParallelWaveGANGenerator(torch.nn.Module):
         self.layers = layers
         self.stacks = stacks
         self.kernel_size = kernel_size
-        
+
         # check the number of layers and stacks
         assert layers % stacks == 0
         layers_per_stack = layers // stacks
-        
+
         # define first convolution
         self.first_conv = Conv1d1x1(in_channels, residual_channels, bias=True)
-        
+
         # define conv + upsampling network
         if upsample_conditional_features:
             upsample_params.update({
@@ -87,7 +87,7 @@ class ParallelWaveGANGenerator(torch.nn.Module):
             self.upsample_net = getattr(upsample, upsample_net)(**upsample_params)
         else:
             self.upsample_net = None
-        
+
         # define residual blocks
         self.conv_layers = torch.nn.ModuleList()
         for layer in range(layers):
@@ -104,7 +104,7 @@ class ParallelWaveGANGenerator(torch.nn.Module):
                 use_causal_conv=use_causal_conv,
             )
             self.conv_layers += [conv]
-        
+
         # define output layers
         self.last_conv_layers = torch.nn.ModuleList([
             torch.nn.ReLU(inplace=True),
@@ -112,11 +112,11 @@ class ParallelWaveGANGenerator(torch.nn.Module):
             torch.nn.ReLU(inplace=True),
             Conv1d1x1(skip_channels, out_channels, bias=True),
         ])
-        
+
         # apply weight norm
         if use_weight_norm:
             self.apply_weight_norm()
-    
+
     def forward(self, x, c):
         """Calculate forward propagation.
 
@@ -128,13 +128,11 @@ class ParallelWaveGANGenerator(torch.nn.Module):
             Tensor: Output tensor (B, out_channels, T)
 
         """
-        B, _, T = x.size()
-        
         # perform upsampling
         if c is not None and self.upsample_net is not None:
             c = self.upsample_net(c)
             assert c.size(-1) == x.size(-1)
-        
+
         # encode to hidden representation
         x = self.first_conv(x)
         skips = 0
@@ -142,36 +140,34 @@ class ParallelWaveGANGenerator(torch.nn.Module):
             x, h = f(x, c)
             skips += h
         skips *= math.sqrt(1.0 / len(self.conv_layers))
-        
+
         # apply final layers
         x = skips
         for f in self.last_conv_layers:
             x = f(x)
-        
+
         return x
-    
+
     def remove_weight_norm(self):
         """Remove weight normalization module from all of the layers."""
-        
         def _remove_weight_norm(m):
             try:
-                logging.debug(f"weight norm is removed from {m}.")
+                logging.debug(f"Weight norm is removed from {m}.")
                 torch.nn.utils.remove_weight_norm(m)
             except ValueError:  # this module didn't have weight norm
                 return
-        
+
         self.apply(_remove_weight_norm)
-    
+
     def apply_weight_norm(self):
         """Apply weight normalization module from all of the layers."""
-        
         def _apply_weight_norm(m):
             if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.Conv2d):
                 torch.nn.utils.weight_norm(m)
-                logging.debug(f"weight norm is applied to {m}.")
-        
+                logging.debug(f"Weight norm is applied to {m}.")
+
         self.apply(_apply_weight_norm)
-    
+
     @staticmethod
     def _get_receptive_field_size(layers, stacks, kernel_size,
                                   dilation=lambda x: 2 ** x):
@@ -179,147 +175,16 @@ class ParallelWaveGANGenerator(torch.nn.Module):
         layers_per_cycle = layers // stacks
         dilations = [dilation(i % layers_per_cycle) for i in range(layers)]
         return (kernel_size - 1) * sum(dilations) + 1
-    
+
     @property
     def receptive_field_size(self):
         """Return receptive field size."""
         return self._get_receptive_field_size(self.layers, self.stacks, self.kernel_size)
 
 
-class ResidualParallelWaveGANDiscriminator(torch.nn.Module):
-    """Parallel WaveGAN Discriminator module."""
-    
-    def __init__(self,
-                 in_channels=1,
-                 out_channels=1,
-                 kernel_size=3,
-                 layers=30,
-                 stacks=3,
-                 residual_channels=64,
-                 gate_channels=128,
-                 skip_channels=64,
-                 dropout=0.0,
-                 use_weight_norm=True,
-                 use_causal_conv=False,
-                 nonlinear_activation_params={"negative_slope": 0.2},
-                 ):
-        """Initialize Parallel WaveGAN Discriminator module.
-
-        Args:
-            in_channels (int): Number of input channels.
-            out_channels (int): Number of output channels.
-            kernel_size (int): Kernel size of dilated convolution.
-            layers (int): Number of residual block layers.
-            stacks (int): Number of stacks i.e., dilation cycles.
-            residual_channels (int): Number of channels in residual conv.
-            gate_channels (int):  Number of channels in gated conv.
-            skip_channels (int): Number of channels in skip conv.
-            dropout (float): Dropout rate. 0.0 means no dropout applied.
-            use_weight_norm (bool): Whether to use weight norm.
-                If set to true, it will be applied to all of the conv layers.
-            use_causal_conv (bool): Whether to use causal structure.
-            nonlinear_activation_params (dict): Nonlinear function parameters
-
-        """
-        super(ResidualParallelWaveGANDiscriminator, self).__init__()
-        assert (kernel_size - 1) % 2 == 0, "Not support even number kernel size."
-        
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.layers = layers
-        self.stacks = stacks
-        self.kernel_size = kernel_size
-        
-        # check the number of layers and stacks
-        assert layers % stacks == 0
-        layers_per_stack = layers // stacks
-        
-        # define first convolution
-        self.first_conv = Conv1d1x1(in_channels, residual_channels, bias=True)
-        self.first_conv_activation = torch.nn.LeakyReLU(
-            negative_slope=nonlinear_activation_params['negative_slope'],
-            inplace=True)
-        
-        # define residual blocks
-        self.conv_layers = torch.nn.ModuleList()
-        for layer in range(layers):
-            dilation = 2 ** (layer % layers_per_stack)
-            conv = ResidualBlock(
-                kernel_size=kernel_size,
-                residual_channels=residual_channels,
-                gate_channels=gate_channels,
-                skip_channels=skip_channels,
-                aux_channels=-1,
-                dilation=dilation,
-                dropout=dropout,
-                bias=True,  # NOTE: magenda uses bias, but musyoku doesn't
-                use_causal_conv=use_causal_conv,
-            )
-            self.conv_layers += [conv]
-        
-        # define output layers
-        self.last_conv_layers = torch.nn.ModuleList([
-            torch.nn.LeakyReLU(negative_slope=nonlinear_activation_params['negative_slope'], inplace=True),
-            Conv1d1x1(skip_channels, skip_channels, bias=True),
-            torch.nn.LeakyReLU(negative_slope=nonlinear_activation_params['negative_slope'], inplace=True),
-            Conv1d1x1(skip_channels, out_channels, bias=True),
-        ])
-        
-        # apply weight norm
-        if use_weight_norm:
-            self.apply_weight_norm()
-    
-    def forward(self, x):
-        """Calculate forward propagation.
-
-        Args:
-            x (Tensor): Input noise signal (B, 1, T).
-
-        Returns:
-            Tensor: Output tensor (B, 1, T)
-
-        """
-        x = self.first_conv(x)
-        x = self.first_conv_activation(x)
-        
-        skips = 0
-        for f in self.conv_layers:
-            x, h = f(x, None)
-            skips += h
-        skips *= math.sqrt(1.0 / len(self.conv_layers))
-        
-        # apply final layers
-        x = skips
-        for f in self.last_conv_layers:
-            x = f(x)
-        return x
-    
-    def apply_weight_norm(self):
-        """Apply weight normalization module from all of the layers."""
-        
-        def _apply_weight_norm(m):
-            if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.Conv2d):
-                torch.nn.utils.weight_norm(m)
-                logging.debug(f"weight norm is applied to {m}.")
-        
-        self.apply(_apply_weight_norm)
-    
-    def remove_weight_norm(self):
-        """Remove weight normalization module from all of the layers."""
-        
-        def _remove_weight_norm(m):
-            try:
-                logging.debug(f"weight norm is removed from {m}.")
-                torch.nn.utils.remove_weight_norm(m)
-            except ValueError:  # this module didn't have weight norm
-                return
-        
-        self.apply(_remove_weight_norm)
-
-
 class ParallelWaveGANDiscriminator(torch.nn.Module):
     """Parallel WaveGAN Discriminator module."""
-    
+
     def __init__(self,
                  in_channels=1,
                  out_channels=1,
@@ -369,11 +234,11 @@ class ParallelWaveGANDiscriminator(torch.nn.Module):
             conv_in_channels, out_channels,
             kernel_size=kernel_size, padding=padding, bias=bias)
         self.conv_layers += [last_conv_layer]
-        
+
         # apply weight norm
         if use_weight_norm:
             self.apply_weight_norm()
-    
+
     def forward(self, x):
         """Calculate forward propagation.
 
@@ -387,25 +252,152 @@ class ParallelWaveGANDiscriminator(torch.nn.Module):
         for f in self.conv_layers:
             x = f(x)
         return x
-    
+
     def apply_weight_norm(self):
         """Apply weight normalization module from all of the layers."""
-        
         def _apply_weight_norm(m):
             if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.Conv2d):
                 torch.nn.utils.weight_norm(m)
-                logging.debug(f"weight norm is applied to {m}.")
-        
+                logging.debug(f"Weight norm is applied to {m}.")
+
         self.apply(_apply_weight_norm)
-    
+
     def remove_weight_norm(self):
         """Remove weight normalization module from all of the layers."""
-        
         def _remove_weight_norm(m):
             try:
-                logging.debug(f"weight norm is removed from {m}.")
+                logging.debug(f"Weight norm is removed from {m}.")
                 torch.nn.utils.remove_weight_norm(m)
             except ValueError:  # this module didn't have weight norm
                 return
-        
+
+        self.apply(_remove_weight_norm)
+
+
+class ResidualParallelWaveGANDiscriminator(torch.nn.Module):
+    """Parallel WaveGAN Discriminator module."""
+
+    def __init__(self,
+                 in_channels=1,
+                 out_channels=1,
+                 kernel_size=3,
+                 layers=30,
+                 stacks=3,
+                 residual_channels=64,
+                 gate_channels=128,
+                 skip_channels=64,
+                 dropout=0.0,
+                 use_weight_norm=True,
+                 use_causal_conv=False,
+                 nonlinear_activation_params={"negative_slope": 0.2},
+                 ):
+        """Initialize Parallel WaveGAN Discriminator module.
+
+        Args:
+            in_channels (int): Number of input channels.
+            out_channels (int): Number of output channels.
+            kernel_size (int): Kernel size of dilated convolution.
+            layers (int): Number of residual block layers.
+            stacks (int): Number of stacks i.e., dilation cycles.
+            residual_channels (int): Number of channels in residual conv.
+            gate_channels (int):  Number of channels in gated conv.
+            skip_channels (int): Number of channels in skip conv.
+            dropout (float): Dropout rate. 0.0 means no dropout applied.
+            use_weight_norm (bool): Whether to use weight norm.
+                If set to true, it will be applied to all of the conv layers.
+            use_causal_conv (bool): Whether to use causal structure.
+            nonlinear_activation_params (dict): Nonlinear function parameters
+
+        """
+        super(ResidualParallelWaveGANDiscriminator, self).__init__()
+        assert (kernel_size - 1) % 2 == 0, "Not support even number kernel size."
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.layers = layers
+        self.stacks = stacks
+        self.kernel_size = kernel_size
+
+        # check the number of layers and stacks
+        assert layers % stacks == 0
+        layers_per_stack = layers // stacks
+
+        # define first convolution
+        self.first_conv = Conv1d1x1(in_channels, residual_channels, bias=True)
+        self.first_conv_activation = torch.nn.LeakyReLU(
+            negative_slope=nonlinear_activation_params['negative_slope'],
+            inplace=True)
+
+        # define residual blocks
+        self.conv_layers = torch.nn.ModuleList()
+        for layer in range(layers):
+            dilation = 2 ** (layer % layers_per_stack)
+            conv = ResidualBlock(
+                kernel_size=kernel_size,
+                residual_channels=residual_channels,
+                gate_channels=gate_channels,
+                skip_channels=skip_channels,
+                aux_channels=-1,
+                dilation=dilation,
+                dropout=dropout,
+                bias=True,  # NOTE: magenda uses bias, but musyoku doesn't
+                use_causal_conv=use_causal_conv,
+            )
+            self.conv_layers += [conv]
+
+        # define output layers
+        self.last_conv_layers = torch.nn.ModuleList([
+            torch.nn.LeakyReLU(negative_slope=nonlinear_activation_params['negative_slope'], inplace=True),
+            Conv1d1x1(skip_channels, skip_channels, bias=True),
+            torch.nn.LeakyReLU(negative_slope=nonlinear_activation_params['negative_slope'], inplace=True),
+            Conv1d1x1(skip_channels, out_channels, bias=True),
+        ])
+
+        # apply weight norm
+        if use_weight_norm:
+            self.apply_weight_norm()
+
+    def forward(self, x):
+        """Calculate forward propagation.
+
+        Args:
+            x (Tensor): Input noise signal (B, 1, T).
+
+        Returns:
+            Tensor: Output tensor (B, 1, T)
+
+        """
+        x = self.first_conv(x)
+        x = self.first_conv_activation(x)
+
+        skips = 0
+        for f in self.conv_layers:
+            x, h = f(x, None)
+            skips += h
+        skips *= math.sqrt(1.0 / len(self.conv_layers))
+
+        # apply final layers
+        x = skips
+        for f in self.last_conv_layers:
+            x = f(x)
+        return x
+
+    def apply_weight_norm(self):
+        """Apply weight normalization module from all of the layers."""
+        def _apply_weight_norm(m):
+            if isinstance(m, torch.nn.Conv1d) or isinstance(m, torch.nn.Conv2d):
+                torch.nn.utils.weight_norm(m)
+                logging.debug(f"Weight norm is applied to {m}.")
+
+        self.apply(_apply_weight_norm)
+
+    def remove_weight_norm(self):
+        """Remove weight normalization module from all of the layers."""
+        def _remove_weight_norm(m):
+            try:
+                logging.debug(f"Weight norm is removed from {m}.")
+                torch.nn.utils.remove_weight_norm(m)
+            except ValueError:  # this module didn't have weight norm
+                return
+
         self.apply(_remove_weight_norm)
