bf36ace49d523d77241680576ef933bc423b0e67
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-02-28 23:20:17 +0900

    fixed hard coding of stats.h5

--- egs/jnas/voc1/run.sh
+++ egs/jnas/voc1/run.sh
@@ -48,6 +48,7 @@ if [ "${stage}" -le 0 ] && [ "${stop_stage}" -ge 0 ]; then
         "${db_root}" data conf/train_speakers.txt
 fi
 
+stats=$(grep -q "hdf5" <(yq ".format" "${conf}") && echo "stats.h5" || echo "stats.npy")
 if [ "${stage}" -le 1 ] && [ "${stop_stage}" -ge 1 ]; then
     echo "Stage 1: Feature extraction"
     # extract raw features
@@ -90,7 +91,7 @@ if [ "${stage}" -le 1 ] && [ "${stop_stage}" -ge 1 ]; then
         ${train_cmd} --num-threads "${n_jobs}" "${dumpdir}/${name}/norm/normalize.log" \
             parallel-wavegan-normalize \
                 --config "${conf}" \
-                --stats "${dumpdir}/${train_set}/stats.h5" \
+                --stats "${dumpdir}/${train_set}/${stats}" \
                 --rootdir "${dumpdir}/${name}/raw" \
                 --dumpdir "${dumpdir}/${name}/norm" \
                 --n_jobs "${n_jobs}" \
@@ -112,7 +113,7 @@ fi
 if [ "${stage}" -le 2 ] && [ "${stop_stage}" -ge 2 ]; then
     echo "Stage 2: Network training"
     [ ! -e "${expdir}" ] && mkdir -p "${expdir}"
-    cp "${dumpdir}/${train_set}/stats.h5" "${expdir}"
+    cp "${dumpdir}/${train_set}/${stats}" "${expdir}"
     if [ "${n_gpus}" -gt 1 ]; then
         train="python -m parallel_wavegan.distributed.launch --nproc_per_node ${n_gpus} -c parallel-wavegan-train"
     else
