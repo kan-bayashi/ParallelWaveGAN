b17e945c1fc5f46fcc14bd6092ad9f1044c49747
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-03-16 15:41:35 +0900

    segments support

--- egs/libritts/voc1/local/data_prep.sh
+++ egs/libritts/voc1/local/data_prep.sh
@@ -12,11 +12,13 @@
 db_root=$1
 part=$2
 data_dir=$3
+db_label_root=$4
 
 # check arguments
-if [ $# != 3 ]; then
-    echo "Usage: $0 [Options] <db_root> <part> <data_dir>"
+if [ $# -lt 3 ] || [ $# -gt 4 ]; then
+    echo "Usage: $0 [Options] <db_root> <part> <data_dir> [<db_label_root>]"
     echo "e.g.: $0 downloads/LibriTTS train-clean-100 data"
+    echo "e.g.: $0 downloads/LibriTTS train-clean-100 data downloads/LibriTTSLabel"
     exit 1
 fi
 
@@ -30,14 +32,54 @@ set -euo pipefail
 
 # set filenames
 scp="${data_dir}/${part}/wav.scp"
+if [ -n "${db_label_root}" ]; then
+    use_segments=true
+    segments="${data_dir}/${part}/segments"
+else
+    use_segments=false
+fi
 
 # check file existence
 [ -e "${scp}" ] && rm "${scp}"
+if "${use_segments}"; then
+    [ -e "${segments}" ] && rm "${segments}"
+fi
 
 # make scp and segments
 find "${db_root}/${part}" -follow -name "*.wav" | sort | while read -r wav; do
     id=$(basename "${wav}" | sed -e "s/\.[^\.]*$//g")
+    lab=$(echo "${wav}" | sed -e "s;${db_root}/${part};${db_label_root}/lab/phone/${part};g" -e "s/.wav/.lab/g")
+
+    # check lab existence
+    if "${use_segments}" && [ ! -e "${lab}" ]; then
+        echo "${id} does not have a label file. skipped."
+        continue
+    fi
+
     echo "${id} ${wav}" >> "${scp}"
+
+    if "${use_segments}"; then
+        # parse label
+        idx=1
+        while true; do
+            symbol=$(sed -n "${idx}p" "${lab}" | awk '{print $3}')
+            if [ "${symbol}" != "sil" ]; then
+                start_sec=$(sed -n "${idx}p" "${lab}" | awk '{print $1}')
+                break
+            fi
+            idx=$((idx+1))
+        done
+        idx=$(wc -l < "${lab}")
+        while true; do
+            symbol=$(sed -n "${idx}p" "${lab}" | awk '{print $3}')
+            if [ -n "${symbol}" ] && [ "${symbol}" != "sp" ]; then
+                end_sec=$(sed -n "${idx}p" "${lab}" | awk '{print $2}')
+                break
+            fi
+            idx=$((idx-1))
+        done
+        echo "${id} ${id} ${start_sec} ${end_sec}" >> "${segments}"
+    fi
 done
 
 echo "Successfully prepared ${part} data."
