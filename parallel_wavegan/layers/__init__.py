d3e27d342eac7c87a56dd0e4898f5143cae98eed
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-05-15 23:05:18 +0900

    added causal conv layers

--- parallel_wavegan/layers/__init__.py
+++ parallel_wavegan/layers/__init__.py
@@ -1,3 +1,4 @@
+from parallel_wavegan.layers.causal_conv import *  # NOQA
 from parallel_wavegan.layers.residual_block import *  # NOQA
 from parallel_wavegan.layers.residual_stack import *  # NOQA
 from parallel_wavegan.layers.upsample import *  # NOQA
