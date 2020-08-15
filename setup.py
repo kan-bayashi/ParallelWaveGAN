a53f30792159b72e2f5c2080f6b85a958a4f9db4
Tomoki Hayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-08-15 18:17:46 +0900

    updated version

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ extras_require = {k: v for k, v in requirements.items()
 
 dirname = os.path.dirname(__file__)
 setup(name="parallel_wavegan",
-      version="0.4.1",
+      version="0.4.2",
       url="http://github.com/kan-bayashi/ParallelWaveGAN",
       author="Tomoki Hayashi",
       author_email="hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp",
