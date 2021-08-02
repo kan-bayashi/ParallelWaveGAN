a76ad7e7c344e4dfc251561638150f52c8f8f6e6
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-02 19:21:41 +0900

    added adversarial_loss

---
+++ parallel_wavegan/losses/adversarial_loss.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 Tomoki Hayashi
+#  MIT License (https://opensource.org/licenses/MIT)
+
+"""Adversarial loss modules."""
+
+import torch
+import torch.nn.functional as F
+
+
+class GeneratorAdversarialLoss(torch.nn.Module):
+    """Generator adversarial loss module."""
+
+    def __init__(
+        self,
+        average_by_discriminators=True,
+    ):
+        """Initialize GeneratorAversarialLoss module."""
+        super().__init__()
+        self.average_by_discriminators = average_by_discriminators
+
+    def forward(self, outputs):
+        """Calcualate feature matching loss.
+
+        Args:
+            outputs (Tensor or list): Discriminator outputs or list of
+                discriminator outputs.
+
+        Returns:
+            Tensor: Generator adversarial loss value.
+
+        """
+        if isinstance(outputs, list):
+            adv_loss = 0.0
+            for i, outputs_ in enumerate(outputs):
+                if isinstance(outputs_, list):
+                    outputs_ = outputs_[-1]
+                adv_loss += F.mse_loss(outputs_, outputs_.new_ones(outputs_.size()))
+            if self.average_by_discriminators:
+                adv_loss /= i + 1
+        else:
+            adv_loss = F.mse_loss(outputs, outputs.new_ones(outputs.size()))
+
+        return adv_loss
+
+
+class DiscriminatorAdversarialLoss(torch.nn.Module):
+    """Discriminator adversarial loss module."""
+
+    def __init__(
+        self,
+        average_by_discriminators=True,
+    ):
+        """Initialize DiscriminatorAversarialLoss module."""
+        super().__init__()
+        self.average_by_discriminators = average_by_discriminators
+
+    def forward(self, outputs_hat, outputs):
+        """Calcualate feature matching loss.
+
+        Args:
+            outputs_hat (Tensor or list): Discriminator outputs or list of
+                discriminator outputs calculated from generator outputs.
+            outputs (Tensor or list): Discriminator outputs or list of
+                discriminator outputs calculated from groundtruth.
+
+        Returns:
+            Tensor: Discriminator real loss value.
+            Tensor: Discriminator fake loss value.
+
+        """
+        if isinstance(outputs, list):
+            real_loss = 0.0
+            fake_loss = 0.0
+            for i, (outputs_hat_, outputs_) in enumerate(zip(outputs_hat, outputs)):
+                if isinstance(outputs_hat_, list):
+                    outputs_hat_ = outputs_hat_[-1]
+                    outputs_ = outputs_[-1]
+                real_loss += F.mse_loss(
+                    outputs_,
+                    outputs_.new_ones(outputs_.size()),
+                )
+                fake_loss += F.mse_loss(
+                    outputs_hat_,
+                    outputs_hat_.new_zeros(outputs_hat_.size()),
+                )
+            if self.average_by_discriminators:
+                fake_loss /= i + 1
+                real_loss /= i + 1
+        else:
+            real_loss = F.mse_loss(
+                outputs,
+                outputs.new_ones(outputs.size()),
+            )
+            fake_loss = F.mse_loss(
+                outputs_hat,
+                outputs_hat.new_zeros(outputs_hat.size()),
+            )
+
+        return real_loss, fake_loss
