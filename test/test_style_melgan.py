b442f04f26777caea454bca3aa40126a7637ccb4
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-03 13:32:50 +0900

    updated test

--- test/test_style_melgan.py
+++ test/test_style_melgan.py
@@ -11,9 +11,13 @@ import numpy as np
 import pytest
 import torch
 
+from parallel_wavegan.losses import DiscriminatorAdversarialLoss
 from parallel_wavegan.losses import GeneratorAdversarialLoss
+from parallel_wavegan.losses import MultiResolutionSTFTLoss
 from parallel_wavegan.models import StyleMelGANDiscriminator
 from parallel_wavegan.models import StyleMelGANGenerator
+
+from test_parallel_wavegan import make_mutli_reso_stft_loss_args
 
 
 logging.basicConfig(
@@ -22,7 +26,7 @@ logging.basicConfig(
 )
 
 
-def make_sytle_melgan_generator_args(**kwargs):
+def make_style_melgan_generator_args(**kwargs):
     defaults = dict(
         in_channels=128,
         aux_channels=80,
@@ -42,7 +46,7 @@ def make_sytle_melgan_generator_args(**kwargs):
     return defaults
 
 
-def make_sytle_melgan_discriminator_args(**kwargs):
+def make_style_melgan_discriminator_args(**kwargs):
     defaults = dict(
         repeats=2,
         window_sizes=[512, 1024, 2048, 4096],
@@ -73,7 +77,6 @@ def make_sytle_melgan_discriminator_args(**kwargs):
 @pytest.mark.parametrize(
     "dict_d",
     [
-        {},
         {"repeats": 1},
         {"repeats": 4},
     ],
@@ -81,7 +84,7 @@ def make_sytle_melgan_discriminator_args(**kwargs):
 def test_style_melgan_discriminator(dict_d):
     batch_size = 4
     batch_length = 2 ** 14
-    args_d = make_sytle_melgan_discriminator_args(**dict_d)
+    args_d = make_style_melgan_discriminator_args(**dict_d)
     y = torch.randn(batch_size, 1, batch_length)
     model_d = StyleMelGANDiscriminator(**args_d)
     gen_adv_criterion = GeneratorAdversarialLoss()
@@ -93,12 +96,15 @@ def test_style_melgan_discriminator(dict_d):
     "dict_g",
     [
         {},
+        {"noise_upsample_scales": [4, 4, 4]},
     ],
 )
 def test_style_melgan_generator(dict_g):
+    args_g = make_style_melgan_generator_args(**dict_g)
     batch_size = 4
-    batch_length = 22528
-    args_g = make_sytle_melgan_generator_args(**dict_g)
+    batch_length = np.prod(args_g["noise_upsample_scales"]) * np.prod(
+        args_g["upsample_scales"]
+    )
     z = torch.randn(batch_size, args_g["in_channels"], 1)
     c = torch.randn(
         batch_size,
@@ -106,4 +112,63 @@ def test_style_melgan_generator(dict_g):
         batch_length // np.prod(args_g["upsample_scales"]),
     )
     model_g = StyleMelGANGenerator(**args_g)
-    model_g(z, c)
+    model_g(c, z)
+
+    # inference
+    c = torch.randn(
+        512,
+        args_g["aux_channels"],
+    )
+    y = model_g.inference(c)
+    print(y.shape)
+
+
+@pytest.mark.parametrize(
+    "dict_g, dict_d, dict_loss",
+    [
+        ({}, {}, {}),
+        ({"noise_upsample_scales": [4, 4, 4]}, {}, {}),
+    ],
+)
+def test_style_melgan_trainable(dict_g, dict_d, dict_loss):
+    # setup
+    args_g = make_style_melgan_generator_args(**dict_g)
+    args_d = make_style_melgan_discriminator_args(**dict_d)
+    args_loss = make_mutli_reso_stft_loss_args(**dict_loss)
+    batch_size = 4
+    batch_length = np.prod(args_g["noise_upsample_scales"]) * np.prod(
+        args_g["upsample_scales"]
+    )
+    y = torch.randn(batch_size, 1, batch_length)
+    c = torch.randn(
+        batch_size,
+        args_g["aux_channels"],
+        batch_length // np.prod(args_g["upsample_scales"]),
+    )
+    model_g = StyleMelGANGenerator(**args_g)
+    model_d = StyleMelGANDiscriminator(**args_d)
+    aux_criterion = MultiResolutionSTFTLoss(**args_loss)
+    gen_adv_criterion = GeneratorAdversarialLoss()
+    dis_adv_criterion = DiscriminatorAdversarialLoss()
+    optimizer_g = torch.optim.Adam(model_g.parameters())
+    optimizer_d = torch.optim.Adam(model_d.parameters())
+
+    # check generator trainable
+    y_hat = model_g(c)
+    p_hat = model_d(y_hat)
+    adv_loss = gen_adv_criterion(p_hat)
+    sc_loss, mag_loss = aux_criterion(y_hat, y)
+    aux_loss = sc_loss + mag_loss
+    loss_g = adv_loss + aux_loss
+    optimizer_g.zero_grad()
+    loss_g.backward()
+    optimizer_g.step()
+
+    # check discriminator trainable
+    p = model_d(y)
+    p_hat = model_d(y_hat.detach())
+    real_loss, fake_loss = dis_adv_criterion(p_hat, p)
+    loss_d = real_loss + fake_loss
+    optimizer_d.zero_grad()
+    loss_d.backward()
+    optimizer_d.step()
