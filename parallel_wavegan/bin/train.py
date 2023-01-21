62293e8879a32261f1f201fac74405cce6b2e0f6
ftshijt <ftshijt@gmail.com>
2023-01-21 05:51:58 -0500

    add pad list function

--- parallel_wavegan/bin/train.py
+++ parallel_wavegan/bin/train.py
@@ -852,6 +852,35 @@ class Collater(object):
 
         return x, c
 
+    def _pad_list(self, xs, pad_value):
+        """Perform padding for the list of tensors.
+
+        Args:
+            xs (List): List of Tensors [(T_1, `*`), (T_2, `*`), ..., (T_B, `*`)].
+            pad_value (float): Value for padding.
+
+        Returns:
+            Tensor: Padded tensor (B, Tmax, `*`).
+
+        Examples:
+            >>> x = [torch.ones(4), torch.ones(2), torch.ones(1)]
+            >>> x
+            [tensor([1., 1., 1., 1.]), tensor([1., 1.]), tensor([1.])]
+            >>> pad_list(x, 0)
+            tensor([[1., 1., 1., 1.],
+                    [1., 1., 0., 0.],
+                    [1., 0., 0., 0.]])
+
+        """
+        n_batch = len(xs)
+        max_len = max(x.size(0) for x in xs)
+        pad = xs[0].new(n_batch, max_len, *xs[0].size()[1:]).fill_(pad_value)
+
+        for i in range(n_batch):
+            pad[i, : xs[i].size(0)] = xs[i]
+
+        return pad
+
 
 def main():
     """Run training process."""
