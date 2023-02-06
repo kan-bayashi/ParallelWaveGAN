3d183de98fc90433328b7928d6f19e101eff1c34
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2023-02-06 10:10:56 +0900

    revert

--- parallel_wavegan/utils/utils.py
+++ parallel_wavegan/utils/utils.py
@@ -319,12 +319,12 @@ def load_model(checkpoint, config=None, stats=None):
         parallel_wavegan.models,
         generator_type,
     )
-    # # workaround for typo #295
-    # generator_params = {
-    #     k.replace("upsample_kernal_sizes", "upsample_kernel_sizes"): v
-    #     for k, v in config["generator_params"].items()
-    # }
-    model = model_class(**config["generator_params"])
+    # workaround for typo #295
+    generator_params = {
+        k.replace("upsample_kernal_sizes", "upsample_kernel_sizes"): v
+        for k, v in config["generator_params"].items()
+    }
+    model = model_class(**generator_params)
     model.load_state_dict(
         torch.load(checkpoint, map_location="cpu")["model"]["generator"]
     )
