48cf570b1cc5f99a11c6ce55278f7893ab4a08e0
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2023-03-22 14:17:21 +0900

    remove

--- test.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-python parallel_wavegan/bin/preprocess.py \
-    --config /data3/qt/ParallelWaveGAN/egs/opencpop_f0_exiciation/voc1/conf/hifigan.v1.yaml \
-    --rootdir /data3/qt/ParallelWaveGAN/egs/opencpop_f0_exiciation/voc1/wav_dump \
-    --dumpdir dump
