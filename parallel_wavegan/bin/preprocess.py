227fc13602de041bae31225bbdf069c33d663975
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2023-03-22 14:57:13 +0900

    refactor

--- parallel_wavegan/bin/preprocess.py
+++ parallel_wavegan/bin/preprocess.py
@@ -9,14 +9,12 @@
 import argparse
 import logging
 import os
-from turtle import window_height
 
 import librosa
 import numpy as np
 import soundfile as sf
 import torch
 import torch.fft
-import torchyin
 import yaml
 from tqdm import tqdm
 
@@ -94,145 +92,7 @@ def logmelfilterbank(
         raise ValueError(f"{log_base} is not supported.")
 
 
-class SineGen(torch.nn.Module):
-    """Definition of sine generator
-    SineGen(samp_rate, harmonic_num = 0,
-            sine_amp = 0.1, noise_std = 0.003,
-            voiced_threshold = 0,
-            flag_for_pulse=False)
-
-    samp_rate: sampling rate in Hz
-    harmonic_num: number of harmonic overtones (default 0)
-    sine_amp: amplitude of sine-wavefrom (default 0.1)
-    noise_std: std of Gaussian noise (default 0.003)
-    voiced_thoreshold: F0 threshold for U/V classification (default 0)
-    flag_for_pulse: this SinGen is used inside PulseGen (default False)
-
-    Note: when flag_for_pulse is True, the first time step of a voiced
-        segment is always sin(np.pi) or cos(0)
-    """
-
-    def __init__(
-        self,
-        samp_rate,
-        harmonic_num=0,
-        sine_amp=0.1,
-        noise_std=0.003,
-        voiced_threshold=0,
-        flag_for_pulse=False,
-    ):
-        super(SineGen, self).__init__()
-        self.sine_amp = sine_amp
-        self.noise_std = noise_std
-        self.harmonic_num = harmonic_num
-        self.dim = self.harmonic_num + 1
-        self.sampling_rate = samp_rate
-        self.voiced_threshold = voiced_threshold
-        self.flag_for_pulse = flag_for_pulse
-
-    def _f02uv(self, f0):
-        # generate uv signal
-        uv = torch.ones_like(f0)
-        uv = uv * (f0 > self.voiced_threshold)
-        return uv
-
-    def _f02sine(self, f0_values):
-        """f0_values: (batchsize, length, dim)
-        where dim indicates fundamental tone and overtones
-        """
-        # convert to F0 in rad. The interger part n can be ignored
-        # because 2 * np.pi * n doesn't affect phase
-        rad_values = (f0_values / self.sampling_rate) % 1
-
-        # initial phase noise (no noise for fundamental component)
-        rand_ini = torch.rand(
-            f0_values.shape[0], f0_values.shape[2], device=f0_values.device
-        )
-        rand_ini[:, 0] = 0
-        rad_values[:, 0, :] = rad_values[:, 0, :] + rand_ini
-
-        # instantanouse phase sine[t] = sin(2*pi \sum_i=1 ^{t} rad)
-        if not self.flag_for_pulse:
-            # for normal case
-
-            # To prevent torch.cumsum numerical overflow,
-            # it is necessary to add -1 whenever \sum_k=1^n rad_value_k > 1.
-            # Buffer tmp_over_one_idx indicates the time step to add -1.
-            # This will not change F0 of sine because (x-1) * 2*pi = x *2*pi
-            tmp_over_one = torch.cumsum(rad_values, 1) % 1
-            tmp_over_one_idx = (tmp_over_one[:, 1:, :] - tmp_over_one[:, :-1, :]) < 0
-            cumsum_shift = torch.zeros_like(rad_values)
-            cumsum_shift[:, 1:, :] = tmp_over_one_idx * -1.0
-
-            sines = torch.sin(
-                torch.cumsum(rad_values + cumsum_shift, dim=1) * 2 * np.pi
-            )
-        else:
-            # If necessary, make sure that the first time step of every
-            # voiced segments is sin(pi) or cos(0)
-            # This is used for pulse-train generation
-
-            # identify the last time step in unvoiced segments
-            uv = self._f02uv(f0_values)
-            uv_1 = torch.roll(uv, shifts=-1, dims=1)
-            uv_1[:, -1, :] = 1
-            u_loc = (uv < 1) * (uv_1 > 0)
-
-            # get the instantanouse phase
-            tmp_cumsum = torch.cumsum(rad_values, dim=1)
-            # different batch needs to be processed differently
-            for idx in range(f0_values.shape[0]):
-                temp_sum = tmp_cumsum[idx, u_loc[idx, :, 0], :]
-                temp_sum[1:, :] = temp_sum[1:, :] - temp_sum[0:-1, :]
-                # stores the accumulation of i.phase within
-                # each voiced segments
-                tmp_cumsum[idx, :, :] = 0
-                tmp_cumsum[idx, u_loc[idx, :, 0], :] = temp_sum
-
-            # rad_values - tmp_cumsum: remove the accumulation of i.phase
-            # within the previous voiced segment.
-            i_phase = torch.cumsum(rad_values - tmp_cumsum, dim=1)
-
-            # get the sines
-            sines = torch.cos(i_phase * 2 * np.pi)
-        return sines
-
-    def forward(self, f0):
-        """sine_tensor, uv = forward(f0)
-        input F0: tensor(batchsize=1, length, dim=1)
-                  f0 for unvoiced steps should be 0
-        output sine_tensor: tensor(batchsize=1, length, dim)
-        output uv: tensor(batchsize=1, length, 1)
-        """
-        with torch.no_grad():
-            f0_buf = torch.zeros(f0.shape[0], f0.shape[1], self.dim, device=f0.device)
-            # fundamental component
-            f0_buf[:, :, 0] = f0[:, :, 0]
-            for idx in np.arange(self.harmonic_num):
-                # idx + 2: the (idx+1)-th overtone, (idx+2)-th harmonic
-                f0_buf[:, :, idx + 1] = f0_buf[:, :, 0] * (idx + 2)
-
-            # generate sine waveforms
-            sine_waves = self._f02sine(f0_buf) * self.sine_amp
-
-            # generate uv signal
-            # uv = torch.ones(f0.shape)
-            # uv = uv * (f0 > self.voiced_threshold)
-            uv = self._f02uv(f0)
-
-            # noise: for unvoiced should be similar to sine_amp
-            #        std = self.sine_amp/3 -> max value ~ self.sine_amp
-            # .       for voiced regions is self.noise_std
-            noise_amp = uv * self.noise_std + (1 - uv) * self.sine_amp / 3
-            noise = noise_amp * torch.randn_like(sine_waves)
-
-            # first: set the unvoiced part to 0 by uv
-            # then: additive noise
-            sine_waves = sine_waves * uv + noise
-        return sine_waves, uv, noise
-
-
-def F0(
+def f0(
     audio,
     sampling_rate,
     hop_size=256,
@@ -240,7 +100,7 @@ def F0(
     pitch_min=40,
     pitch_max=10000,
 ):
-    """Compute log-Mel filterbank feature.
+    """Compute F0.
 
     Args:
         audio (ndarray): Audio signal (T,).
@@ -248,13 +108,19 @@ def F0(
         hop_size (int): Hop size.
         pitch_min (int): Minimum pitch in pitch extraction.
         pitch_max (int): Maximum pitch in pitch extraction.
+
     Returns:
         ndarray: f0 feature (#frames, ).
+
+    Note:
+        Unvoiced frame has value = 0.
 
     """
     torch_wav = torch.from_numpy(audio).float()
     if frame_length is not None:
         pitch_min = sampling_rate / (frame_length / 2)
+
+    import torchyin
 
     pitch = torchyin.estimate(
         torch_wav,
@@ -345,6 +211,10 @@ def main():
         config = yaml.load(f, Loader=yaml.Loader)
     config.update(vars(args))
 
+    # check model architecture
+    generator_type = (config.get("generator_type", "ParallelWaveGANGenerator"),)
+    use_f0_and_excitation = generator_type == "UHiFiGANGenerator"
+
     # check arguments
     if (args.wav_scp is not None and args.rootdir is not None) or (
         args.wav_scp is None and args.rootdir is None
@@ -376,7 +246,10 @@ def main():
     else:
         sampling_rate = config["sampling_rate_for_feats"]
 
-    ExcitationExtractor = SineGen(samp_rate=sampling_rate)
+    if use_f0_and_excitation:
+        from parallel_wavegan.layers import SineGen
+
+        ExcitationExtractor = SineGen(samp_rate=sampling_rate)
 
     # process each data
     for utt_id, (audio, fs) in tqdm(dataset):
@@ -437,61 +310,30 @@ def main():
         # make sure the audio length and feature length are matched
         audio = np.pad(audio, (0, config["fft_size"]), mode="reflect")
 
-        f0 = F0(
-            audio,
-            sampling_rate=sampling_rate,
-            hop_size=hop_size,
-            frame_length=config["win_length"],
-        ).reshape(-1, 1)
-        f0 = f0[: len(mel)]
-        extended_f0 = (
-            torch.from_numpy(f0)
-            .reshape(1, 1, -1)
-            .repeat(1, config["hop_size"], 1)
-            .reshape(1, -1, 1)
-        )
-        sine_waves, uv, noise = ExcitationExtractor(extended_f0)
-        # logging.warn(f'sine_waves.shape:{sine_waves.shape}')
-        excitation = (
-            sine_waves.squeeze(0).squeeze(-1).cpu().numpy()
-        )  # .reshape(config["hop_size"],-1).transpose(1,0).cpu().numpy()
-
-        excitation = excitation[: len(mel) * config["hop_size"]]
-        excitation = excitation.reshape(-1, config["hop_size"])  # .squeeze(-1)
-
-        if False:
-            # logging.warn(f'before stft excitation.shape:{excitation.shape}')
-            excitation = librosa.stft(
-                excitation,
-                n_fft=config["fft_size"],
-                hop_length=hop_size,
-                win_length=config["win_length"],
-                window=config["window"],
-                pad_mode="reflect",
-            )
-            # logging.warn(f'after stft excitation.shape:{excitation.shape}')
-            excitation = np.abs(excitation).T  # (#frames, #bins)
-            # logging.warn(f'after abs stft excitation.shape:{excitation.shape}')
-
-            excitation = excitation[: len(mel), :]
-
-        # excitation = excitation.transpose(1,0)
-
-        # logging.warn(f'excitation.shape:{excitation.shape}')
-        # logging.warn(f'f0.shape:{f0.shape}')
-        # logging.warn(f'mel.shape:{mel.shape}')
         # make sure the audio length and feature length are matched
         audio = audio[: len(mel) * config["hop_size"]]
         assert len(mel) * config["hop_size"] == len(audio)
 
-        mel = np.squeeze(mel)
-        f0 = np.squeeze(f0)
-        excitation = np.squeeze(excitation)
-
-        # logging.warn(f'audio:{audio.shape}')
-        # logging.warn(f'mel:{mel.shape}')
-        # logging.warn(f'f0:{f0.shape}')
-        # logging.warn(f'excitation:{excitation.shape}')
+        if use_f0_and_excitation:
+            f0_ = f0(
+                audio,
+                sampling_rate=sampling_rate,
+                hop_size=hop_size,
+                frame_length=config["win_length"],
+            ).reshape(-1, 1)
+            f0_ = f0_[: len(mel)]
+            extended_f0 = (
+                torch.from_numpy(f0_)
+                .reshape(1, 1, -1)
+                .repeat(1, config["hop_size"], 1)
+                .reshape(1, -1, 1)
+            )
+            sine_waves, _, _ = ExcitationExtractor(extended_f0)
+            excitation = sine_waves.squeeze(0).squeeze(-1).cpu().numpy()
+            excitation = excitation[: len(mel) * config["hop_size"]]
+            excitation = excitation.reshape(-1, config["hop_size"])
+            f0_ = np.squeeze(f0_)  # (#frames,)
+            excitation = np.squeeze(excitation)  # (#frames, hop_size)
 
         # apply global gain
         if config["global_gain_scale"] > 0.0:
@@ -515,16 +357,17 @@ def main():
                 "feats",
                 mel.astype(np.float32),
             )
-            write_hdf5(
-                os.path.join(args.dumpdir, f"{utt_id}.h5"),
-                "f0",
-                f0.astype(np.float32),
-            )
-            write_hdf5(
-                os.path.join(args.dumpdir, f"{utt_id}.h5"),
-                "excitation",
-                excitation.astype(np.float32),
-            )
+            if use_f0_and_excitation:
+                write_hdf5(
+                    os.path.join(args.dumpdir, f"{utt_id}.h5"),
+                    "f0",
+                    f0.astype(np.float32),
+                )
+                write_hdf5(
+                    os.path.join(args.dumpdir, f"{utt_id}.h5"),
+                    "excitation",
+                    excitation.astype(np.float32),
+                )
         elif config["format"] == "npy":
             np.save(
                 os.path.join(args.dumpdir, f"{utt_id}-wave.npy"),
@@ -536,16 +379,17 @@ def main():
                 mel.astype(np.float32),
                 allow_pickle=False,
             )
-            np.save(
-                os.path.join(args.dumpdir, f"{utt_id}-f0.npy"),
-                f0.astype(np.float32),
-                allow_pickle=False,
-            )
-            np.save(
-                os.path.join(args.dumpdir, f"{utt_id}-excitation.npy"),
-                excitation.astype(np.float32),
-                allow_pickle=False,
-            )
+            if use_f0_and_excitation:
+                np.save(
+                    os.path.join(args.dumpdir, f"{utt_id}-f0.npy"),
+                    f0.astype(np.float32),
+                    allow_pickle=False,
+                )
+                np.save(
+                    os.path.join(args.dumpdir, f"{utt_id}-excitation.npy"),
+                    excitation.astype(np.float32),
+                    allow_pickle=False,
+                )
         else:
             raise ValueError("support only hdf5 or npy format.")
 
